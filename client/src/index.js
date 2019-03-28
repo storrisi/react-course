b79bb841bd5b553d738e770c6591b25f06020968
Simone Torrisi <storrisi@gmail.com>
2019-03-28 15:08:49 +0100

    Merge fixes

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader'
 import './index.css';
+<<<<<<< Updated upstream
 import App from './App';
+=======
+import App from './containers/App';
+>>>>>>> Stashed changes
 import * as serviceWorker from './serviceWorker';
 
 const render = Component => {
@@ -14,7 +18,11 @@ const render = Component => {
 render(App)
 
 if (module.hot) {
+<<<<<<< Updated upstream
     module.hot.accept('./App', () => {render(App)})
+=======
+    module.hot.accept('./containers/App', () => {render(App)})
+>>>>>>> Stashed changes
 }
 
 
